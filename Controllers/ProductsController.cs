6f499a4812d66e90cee280189698d8e451412681
romanbelowski <roman_belowski@protonmail.com>
2023-11-09 12:46:11 +0300

    Add ability to edit product prop in DB.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -52,6 +52,55 @@ namespace WSLab.Controllers
             return RedirectToAction("Add");
         }
 
+        // Edit/View DB Table
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var product = await dataBaseContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product != null)
+            {
+                var viewModel = new UpdateProductViewModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = product.Name,
+                    Description = product.Description,
+                    Category = product.Category,
+                    Price = product.Price,
+                    Image = product.Image,
+                    AvaibleQuantity = product.AvaibleQuantity
+
+                };
+                return await Task.Run(() =>View("View", viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> View(UpdateProductViewModel updateProductRequest)
+        {
+            var product = await dataBaseContext.Products.FindAsync(updateProductRequest.Id);
+
+            if (product != null)
+            {
+                product.Name = updateProductRequest.Name;
+                product.Description = updateProductRequest.Description;
+                product.Category = updateProductRequest.Category;
+                product.Price = updateProductRequest.Price;
+                product.Image = updateProductRequest.Image;
+                product.AvaibleQuantity = updateProductRequest.AvaibleQuantity;
+
+                await dataBaseContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+
+            }
+
+            return RedirectToAction("Index");
+        }
 
 
 
